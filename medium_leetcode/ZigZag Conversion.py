40d3e54744dc87e71677dcb8cebcac1ea4c8be23
Yaozuwu <yaozuwu279@gmail.com>
2019-07-20 11:22:56 +0800

    2019.7.20

---
+++ medium_leetcode/ZigZag Conversion.py
@@ -0,0 +1,53 @@
+"""
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+"""
+
+class Solution(object):
+    def convert(self, s, numRows):
+        if numRows == 1:
+            return s
+        ldict = {}
+        for i in range(1,numRows+1):
+            ldict[i] = ''
+        n = 1
+        i = 0
+        while i < len(s):
+            while n < numRows:
+                if i == len(s):
+                    break
+                ldict[n] += s[i]
+                n +=1
+                i +=1
+            while n > 1:
+                if i == len(s):
+                    break
+                ldict[n] += s[i]
+                n -= 1
+                i += 1
+        out = ''
+        for i in ldict.values():
+            out +=i 
+        return out
